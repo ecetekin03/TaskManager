9ac16dd996fc3b1f4a08da7f0ed9d8b675c8cdbb
Ece Tekin <150441920+ecetekin03@users.noreply.github.com>
2025-08-12 10:56:41 +0300

    Update server.js

--- TaskTracker/server.js
+++ TaskTracker/server.js
@@ -361,4 +361,9 @@ cron.schedule(
 
 
 // === SERVER START ===
-app.listen(3000, () => console.log("Server http://localhost:3000"));
+//app.listen(3000, () => console.log("Server http://localhost:3000"));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
